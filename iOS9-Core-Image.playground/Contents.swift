b1fbf7e49e5d99d5667d2cc52c341e155b2fd4a1
Paul Solt <PaulSolt@gmail.com>
2019-10-28 12:42:36 -0400

    Added playground

---
+++ iOS9-Core-Image.playground/Contents.swift
@@ -0,0 +1,9 @@
+import CoreImage
+
+//let filter = CIFilter(name: "CIGaussianBlur")!
+let filter = CIFilter(name: "CIColorControls")!
+
+
+
+// See all the details about the filter
+print(filter.attributes)
